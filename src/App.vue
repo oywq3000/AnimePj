eba80fcf55346561ef0bc3c533c8b0b0f74ad081
oywq2020 <2603321762@qq.com>
2024-09-01 21:17:52 +0800

    construct pj

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+   <IfDome/>
+   <ListShow/>
+</template>
+
+
+<script setup>
+import ListShow from '@/components/ListShow.vue'
+import IfDome from '@/components/IfDome.vue'
+
+</script>
+
+
+
---
+++ src/components/ListShow.vue
@@ -0,0 +1,12 @@
+<template>
+    <p v-for="item in list">{{item}}</p>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            list:["1","2","3"]
+        }
+    }
+}
+</script>
