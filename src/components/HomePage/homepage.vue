b82409dd515b7e5ba3d1219c04be063576069761
oywq2020 <2603321762@qq.com>
2024-09-05 21:37:17 +0800

    complete the basic construction of the leftentry

--- src/components/HomePage/homepage.vue
+++ src/components/HomePage/homepage.vue
@@ -2,9 +2,9 @@
   <div class="header_bar">
     <div class="header_min_bar_top">
     <ul class="right_entry">
-        <li>首页</li>
-        <li>番剧</li>
-        <li>直播</li>
+        <li class="liDisplay"><a href="">首页</a></li>
+        <li class="liDisplay">番剧</li>
+        <li class="liDisplay">直播</li>
     </ul>
     </div>
   </div>
@@ -34,11 +34,21 @@
   width: 1920px;
   height: 70px;
 }
-.rightentry{
+.right_entry{
     background: rgb(202, 222, 194);
   width: 487px;
   height: 70px;
   margin-left:30px;
+  margin-top: 0;
+  margin-bottom: 0;
   padding: 0;
+  display: flex;
+ float: left;
+}
+.liDisplay{
+  display: inline;
+  align-self: center;
+  margin: 20px;
+
 }
 </style>
